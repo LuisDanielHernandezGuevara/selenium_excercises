ebc5096d9ff20b5b01978a587a4b0d3a9ae6e817
1000075142 <1000075142@Maverick-VM-11.corp.hexaware.com>
2022-07-19 10:19:09 -0500

    75142_changes

--- src/main/java/Introduction/excercise/DinamicDropdown.java
+++ src/main/java/Introduction/excercise/DinamicDropdown.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DinamicDropdown {
 
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\1000075142\\Documents\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
